a48fc62a305cfb3b00f5e9440c2a8ab09be13bdb
Craig George <cgeorge@bmgfleet.com>
2026-02-14 00:25:43 -0600

    fix: use standard supabase-js client to fix AbortError on all queries

--- src/lib/supabase-browser.ts
+++ src/lib/supabase-browser.ts
@@ -1,11 +1,11 @@
-import { createClient as createSupabaseClient } from '@supabase/supabase-js';
+import { createClient as createSupabaseClient, SupabaseClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-let client: ReturnType<typeof createSupabaseClient> | null = null;
+let client: SupabaseClient | null = null;
 
-export function createClient() {
+export function createClient(): SupabaseClient<any, 'public', any> {
   if (client) return client;
 
   client = createSupabaseClient(supabaseUrl, supabaseAnonKey, {
